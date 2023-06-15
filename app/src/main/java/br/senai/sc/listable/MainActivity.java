9a9dcd19f382a80af109db29af91e5b7789b1b4a
LUCAS ALVISI MADER <lucas_a_mader@estudante.sesisenai.org.br>
2023-06-14 21:44:16 -0300

    finished-front-login

--- app/src/main/java/br/senai/sc/listable/MainActivity.java
+++ app/src/main/java/br/senai/sc/listable/MainActivity.java
@@ -2,13 +2,19 @@ package br.senai.sc.listable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        TextView textView = findViewById(R.id.login_register_text);
+        String html = "<font color=#1C1C1C>Não possui conta? </font><font color=#FD9226>Registre-se</font>";
+        textView.setText(Html.fromHtml(html));
     }
 }
