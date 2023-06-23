1ac128fe3e9ede178d5beb737efd8819927e9ba0
Lucas <lucas_a_mader@estudante.sesisenai.org.br>
2023-06-22 21:35:04 -0300

    header_fragment

--- app/src/main/java/br/senai/sc/listable/fragment/AddListFragment.java
+++ app/src/main/java/br/senai/sc/listable/fragment/AddListFragment.java
@@ -1,21 +1,19 @@
 package br.senai.sc.listable.fragment;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import br.senai.sc.listable.R;
 
-public class HeaderFragment extends Fragment {
+public class AddListFragment extends Fragment {
 
-    public HeaderFragment() {}
+    public AddListFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.header_fragment, container, false);
+        return inflater.inflate(R.layout.add_list_fragment, container, false);
     }
 }
