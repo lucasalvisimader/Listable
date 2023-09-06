15fce5ed5d1cfafd6085bf3e77e687582f4ec542
Lucas <105462862+Lucas-Mader@users.noreply.github.com>
2023-09-06 20:04:09 -0300

    edit front

--- app/src/main/java/br/senai/sc/listable/recycleView/adapter/AdapterShoppingListItems.java
+++ app/src/main/java/br/senai/sc/listable/recycleView/adapter/AdapterShoppingListItems.java
@@ -4,11 +4,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.icu.text.ListFormatter;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -78,8 +80,9 @@ public class AdapterShoppingListItems extends RecyclerView.Adapter<ShoppingListI
     private void showEditItemFragment(Item item, Context context) {
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.fragment_edit_item);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().setGravity(Gravity.);
         dialog.show();
     }
 }
